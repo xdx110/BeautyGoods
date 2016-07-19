1bb8ac8e3951b8f7f52b9db43ec4bf0342f59a71
许东昡 <402959823@qq.com>
2016-07-19 19:36:32 +0800

    完成p

---
+++ app/src/main/java/com/xdx/dllo/beautygoodsdemo/goods/GoodsPresenter.java
@@ -0,0 +1,36 @@
+package com.xdx.dllo.beautygoodsdemo.goods;
+
+import com.xdx.dllo.beautygoodsdemo.base.BaseContract;
+
+/**
+ * Created by dllo on 16/7/19.
+ */
+public class GoodsPresenter implements BaseContract.Presenter {
+    private BaseContract.Model model;
+    private BaseContract.View view;
+
+    public GoodsPresenter(BaseContract.Model model, BaseContract.View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    @Override
+    public void getUrl(String url) {
+        model.getUrl(url);
+    }
+
+    @Override
+    public void urlSuccess(Object data) {
+        view.getData(data);
+    }
+
+    @Override
+    public void urlError() {
+        view.urlError("请求失败");
+    }
+
+    @Override
+    public void start() {
+        model.setPresenter(this);
+    }
+}
