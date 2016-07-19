3ff55eefe51f979647472e8cd2189ff636ae2bea
dllo <xiaosha3385@126.com>
2016-07-19 19:25:16 +0800

    goods_mo完成了

---
+++ app/src/main/java/com/xdx/dllo/beautygoodsdemo/goods/GoodModel.java
@@ -0,0 +1,33 @@
+package com.xdx.dllo.beautygoodsdemo.goods;
+
+import com.xdx.dllo.beautygoodsdemo.base.BaseContract;
+import com.xdx.dllo.beautygoodsdemo.internet.MyOkHttp;
+
+/**
+ * Created by dllo on 16/7/19.
+ */
+public class GoodModel implements BaseContract.Model {
+    private BaseContract.Presenter presenter;
+
+    @Override
+    public void getUrl(String url) {
+        MyOkHttp.getInstance().getRequestAsync(url, GoodsBean.class, new MyOkHttp.OnTrue<GoodsBean>() {
+            @Override
+            public void hasData(GoodsBean data) {
+                presenter.urlSuccess(data);
+
+            }
+        }, new MyOkHttp.OnError() {
+            @Override
+            public void noHasData() {
+                presenter.urlError();
+            }
+        });
+
+    }
+
+    @Override
+    public void setPresenter(BaseContract.Presenter presenter) {
+        this.presenter = presenter;
+    }
+}
