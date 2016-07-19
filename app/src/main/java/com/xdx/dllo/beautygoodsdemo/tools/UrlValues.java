7dab2259a9058fb7b8cc7c4d13541b7be07819a8
dllo <402959823@qq.com>
2016-07-19 10:29:09 +0800

    建类

---
+++ app/src/main/java/com/xdx/dllo/beautygoodsdemo/tools/UrlValues.java
@@ -0,0 +1,8 @@
+package com.xdx.dllo.beautygoodsdemo.tools;
+
+/**
+ * Created by dllo on 16/7/19.
+ */
+public class UrlValues {
+    public static final String STYLIST_URL="http://design.zuimeia.com/api/v1/designers/?page=1&page_size=30&device_id=863360020709857&platform=android&lang=zh&appVersion=1.0.4&appVersionCode=10004&systemVersion=23&countryCode=CN&user_id=0&token=&package_name=com.zuiapps.zuiworld ";
+}
